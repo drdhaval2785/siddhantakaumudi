b9176a960e0da9ca969612407f3dc8643a64d4a2
drdhaval2785 <drdhaval2785@gmail.com>
2017-01-15 21:05:45 +0530

    renamed sk0_notes.txt

--- redo.sh
+++ redo.sh
@@ -1,5 +1,7 @@
+diff sk0.txt siddhantakaumudi.txt > sk0_notes.txt
 echo "Step 1 - Creating sk1.txt from sk0.txt by adding markup."
 python step1.py sk0.txt sk1.txt step1_notes.txt
+diff sk1.txt sk0.txt > sk1_notes.txt
 echo "Step 2 - Creating sk.xml from sk1.txt file."
 python make_xml.py
 echo "Step 3 - Validating sk.xml file for errors."
