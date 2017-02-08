ee0f3c48e39b8abbd27f6e1ecd90d1ed754da3ac
drdhaval2785 <drdhaval2785@gmail.com>
2017-02-08 15:47:38 +0530

    Near completion for TEI

--- make_xml.py
+++ make_xml.py
@@ -15,9 +15,9 @@ endtext = '</body>\n</text>\n</div></div></div>'
 def revhistory():
 	historydata = [
 		('1.0.0','2016-12-27','First version. 1. There are misorders in this file. So created a list of misordered sUtra numbers by step0.py in step0_notes.txt and made corrections in sk0.txt. 2. Added missing 2139-2150 sUtras manually in sk0.txt. 3. sarvasamAsazeSaprakaraNam is missing.  - Added manually. 4. prakaraNa headings were missing. - Added manually. 5. तिङन्तप्रत्ययमालाप्रकरणम्‌ is missing. - Added manually. 6. Last one portion of svaraprakaraNam page 775 is missing. Added manually. 8. Correct verb number errors. They should be in chronologic order. When not, it means it is wrong. e.g. 157 वगि -> 147 वगि 9. 1209 verb number are missing in original. Made adjustment in step1.py logic. 10. Some missing data for verbs was also incorporated. The diff file is logged in sk0_manual.txt file.','Dr. Dhaval Patel','drdhaval2785@gmail.com'),
-		('1.1.0','2017-01-06','Added missing vArtika markup manually. See https://github.com/drdhaval2785/siddhantakaumudi/issues/4','Dr. Dhaval Patel','drdhaval2785@gmail.com'),
+		('1.1.0','2017-02-06','Added missing vArtika markup manually. See https://github.com/drdhaval2785/siddhantakaumudi/issues/4','Dr. Dhaval Patel','drdhaval2785@gmail.com'),
 	]
-	headertext = '<teiHeader xml:lang="en">\n<fileDesc>\n<titleStmt>\n<title>Siddhantakaumudi</title>\n<respStmt>\n<persName>Dr. Dhaval Patel</persName>\n<resp>Creation and updation of XML</resp>\n</respStmt>\n<respStmt>\n<persName>Dr. H. N. Bhat</persName>\n<resp>Supply of base docx file.</resp>\n</respStmt>\n</titleStmt>\n<publicationStmt>\n<authority>Dr. Dhaval Patel</authority>\n<availability status="open"></availability>\n<date>2017-02-06</date>\n</publicationStmt>\n<notesStmt>\n<note>Dr. H. N. Bhat posted a docx file on a google group on 2016-12-26 at https://groups.google.com/forum/#!searchin/bvparishat/siddhantakaumudi$20unicode|sort:relevance/bvparishat/iYYVe5sFaFM/tPBIEFvcDAAJ. The digital file has no description of source from where it was encoded nor who encoded it.</note>\n</notesStmt>\n<sourceDesc>\n<bibl>\n<title>Siddhantakaumudi</title>\n<publisher>Unknown</publisher>\n<date>Unknown</date>\n</bibl>\n</sourceDesc>\n</fileDesc>\n<encodingDes>\n<p>The e-text is in Devanagari script</p>\n</encodingDes>\n<revisionDesc>\n'
+	headertext = '<teiHeader xml:lang="en">\n<fileDesc>\n<titleStmt>\n<title>Siddhantakaumudi</title>\n<respStmt>\n<persName>Dr. Dhaval Patel</persName>\n<resp>Creation and updation of XML</resp>\n</respStmt>\n<respStmt>\n<persName>Dr. H. N. Bhat</persName>\n<resp>Supply of base docx file.</resp>\n</respStmt>\n</titleStmt>\n<publicationStmt>\n<publisher>https://github.com/drdhaval2785/siddhantakaumudi/</publisher>\n<authority>Dr. Dhaval Patel</authority>\n<availability status="free"></availability>\n<date>2017-02-06</date>\n</publicationStmt>\n<notesStmt>\n<note>Dr. H. N. Bhat posted a docx file on a google group on 2016-12-26 at https://groups.google.com/forum/#!searchin/bvparishat/siddhantakaumudi$20unicode|sort:relevance/bvparishat/iYYVe5sFaFM/tPBIEFvcDAAJ. The digital file has no description of source from where it was encoded nor who encoded it.</note>\n</notesStmt>\n<sourceDesc>\n<bibl>\n<title>Siddhantakaumudi</title>\n<publisher>Unknown</publisher>\n<date>Unknown</date>\n</bibl>\n</sourceDesc>\n</fileDesc>\n<encodingDes>\n<p>The e-text is in Devanagari script</p>\n</encodingDes>\n<revisionDesc>\n'
 	for (version, date, changelog, person, email) in historydata:
 		headertext += '<change who="'+person+'" when="'+date+'">'+version+' - '+changelog+' '+'</change>\n'
 	headertext += '</revisionDesc>\n</teiHeader>\n<text>\n<body>'
@@ -64,42 +64,39 @@ def add_tags(x):
 	x = re.sub(u'</vivaraRam>\n</sUtra>\n<prakaraRAnta>तिङन्तप्रत्ययमालाप्रकरणम्‌</prakaraRAnta>',u'\n<prakaraRAnta>तिङन्तप्रत्ययमालाप्रकरणम्‌</prakaraRAnta>',x)
 	return x
 def add_tags1(x):
-	x = re.sub(r'{#','</div>\n</div>\n<div type="sUtra">\n<div type="SK">',x)
-	x = re.sub(r'#}','</div><div type="sUtramUlam">',x)
-	x = re.sub(r'{@','</div><div type="AS">',x)
-	x = re.sub(r'@}','</div>\n<div type="vivaraRam">',x)
+	x = re.sub(u'(। इति .*प्रकरणम्‌ ।)',u'</p>\n</div>\n<trailer>\g<0></trailer>',x)
+	x = re.sub(r'{#([0-9]+)#}(.*){@([0-9-]+)@}','</p>\n</div>\n<div type="sUtra_with_explanation" n="\g<1>">\n<ab type="sUtra" n="\g<1>"><label type="SK">\g<1></label><label type="AS">\g<3></label>\g<2></ab>\n<p>',x)
+	x = re.sub('</p>\n</div>\n<div type="sUtra_with_explanation" n="1">','<div type="sUtra_with_explanation" n="1">',x)
+	x = re.sub(u'\n(॥ अथ [^॥]+ ॥)','\n<div type="prakaraRa">\n<head>\g<1></head>',x)
+	x = re.sub(u'</head>\W+</p>\W+</div>','</head>\n',x)
 	x = re.sub(r'{[*]','<div type="SKsandarBaH">',x)
 	x = re.sub(r'[*]}','</div>',x)
 	x = re.sub(r'{[$]','<div type="DAtukramaH">',x)
 	x = re.sub(r'[$]}','</div>',x)
 	x = re.sub(r'{[%]([^%]*)[%]}','<div type="vArtika">\g<1></div>',x)
 	x = re.sub(u'।<div type="vArtika">','<div type="vArtika">',x)
-	x = re.sub(u'\n॥ अथ ([^॥]+) ॥','\n<div type="prakaraRa">\n<div type="prakaraRanAman">\g<1></div>\n',x)
-	x = re.sub(u'प्रकरणम्‌</div>\W+</div>\W+</div>\W+<div type="sUtra">',u'प्रकरणम्‌</div>\n<div type="sUtra">',x)
-	x = re.sub(u'प्रकरणम्‌</div>\W+</div>\W+</div>\W+<div type="sUtra">',u'प्रकरणम्‌</div>\n<div type="sUtra">',x)
 	x = x.replace('XXXXXXXXXXXXXXXXXXXX','</div>')
 	x = x.replace('====================================','</div>')
 	x = x.replace('</div></div></div>','</div>')
-	x = re.sub(u'। इति (.*प्रकरणम्‌) ।',u'</div>\n</div>\n<div type="prakaraRAnta">\g<1></div>',x)
-	x = re.sub(u'। इति (.*प्रकरणम्) ।',u'</div>\n</div>\n<div type="prakaraRAnta">\g<1></div>',x)
-	x = x.replace(u'</div>\n</div>\n<div type="sUtra">\n<div type="SK">150</div>','<div type="sUtra">\n<div type="SK">150</div>') # Some odd entries handled now onwards
-	x = x.replace(u'</div>\n</div>\n<div type="sUtra">\n<div type="SK">162</div>','<div type="sUtra">\n<div type="SK">162</div>')
-	x = x.replace(u'</div>\n</div>\n<div type="sUtra">\n<div type="SK">2423</div>','<div type="sUtra">\n<div type="SK">2423</div>')
-	x = x.replace(u'</div>\n</div>\n<div type="sUtra">\n<div type="SK">2489</div>','<div type="sUtra">\n<div type="SK">2489</div>')
-	x = x.replace(u'</div>\n</div>\n<div type="sUtra">\n<div type="SK">2505</div>','<div type="sUtra">\n<div type="SK">2505</div>')
-	x = x.replace(u'</div>\n</div>\n<div type="sUtra">\n<div type="SK">2523</div>','<div type="sUtra">\n<div type="SK">2523</div>')
-	x = x.replace(u'</div>\n</div>\n<div type="sUtra">\n<div type="SK">2534</div>','<div type="sUtra">\n<div type="SK">2534</div>')
-	x = x.replace(u'</div>\n</div>\n<div type="sUtra">\n<div type="SK">2543</div>','<div type="sUtra">\n<div type="SK">2543</div>')
-	x = x.replace(u'</div>\n</div>\n<div type="sUtra">\n<div type="SK">2547</div>','<div type="sUtra">\n<div type="SK">2547</div>')
-	x = x.replace(u'</div>\n</div>\n<div type="sUtra">\n<div type="SK">2554</div>','<div type="sUtra">\n<div type="SK">2554</div>')
-	x = x.replace(u'</div>\n</div>\n<div type="sUtra">\n<div type="SK">2563</div>','<div type="sUtra">\n<div type="SK">2563</div>')
-	x = x.replace(u'</div>\n</div>\n<div type="sUtra">\n<div type="SK">2679</div>','<div type="sUtra">\n<div type="SK">2679</div>')
-	x = x.replace(u'</div>\n</div>\n<div type="prakaraRAnta">सवसमासशेषप्रकरणम्‌</div>',u'<div type="prakaraRAnta">सवसमासशेषप्रकरणम्‌</div>')
+	x = x.replace(u'</p>\n</div>\n<div type="sUtra_with_explanation" n="150">','<div type="sUtra_with_explanation" n="150">') # Some odd entries handled now onwards
+	x = x.replace(u'</p>\n</div>\n<div type="sUtra_with_explanation" n="162">','<div type="sUtra_with_explanation" n="162">') # Some odd entries handled now onwards
+	x = x.replace(u'</p>\n</div>\n<div type="sUtra_with_explanation" n="2423">','<div type="sUtra_with_explanation" n="2423">') # Some odd entries handled now onwards
+	x = x.replace(u'</p>\n</div>\n<div type="sUtra_with_explanation" n="2489">','<div type="sUtra_with_explanation" n="2489">') # Some odd entries handled now onwards
+	x = x.replace(u'</p>\n</div>\n<div type="sUtra_with_explanation" n="2505">','<div type="sUtra_with_explanation" n="2505">') # Some odd entries handled now onwards
+	x = x.replace(u'</p>\n</div>\n<div type="sUtra_with_explanation" n="2523">','<div type="sUtra_with_explanation" n="2523">') # Some odd entries handled now onwards
+	x = x.replace(u'</p>\n</div>\n<div type="sUtra_with_explanation" n="2534">','<div type="sUtra_with_explanation" n="2534">') # Some odd entries handled now onwards
+	x = x.replace(u'</p>\n</div>\n<div type="sUtra_with_explanation" n="2543">','<div type="sUtra_with_explanation" n="2543">') # Some odd entries handled now onwards
+	x = x.replace(u'</p>\n</div>\n<div type="sUtra_with_explanation" n="2547">','<div type="sUtra_with_explanation" n="2547">') # Some odd entries handled now onwards
+	x = x.replace(u'</p>\n</div>\n<div type="sUtra_with_explanation" n="2554">','<div type="sUtra_with_explanation" n="2554">') # Some odd entries handled now onwards
+	x = x.replace(u'</p>\n</div>\n<div type="sUtra_with_explanation" n="2563">','<div type="sUtra_with_explanation" n="2563">') # Some odd entries handled now onwards
+	x = x.replace(u'</p>\n</div>\n<div type="sUtra_with_explanation" n="2679">','<div type="sUtra_with_explanation" n="2679">') # Some odd entries handled now onwards
+	x = x.replace(u'</p>\n</div>\n<trailer>। इति सवसमासशेषप्रकरणम्‌ ।</trailer>',u'<trailer>। इति सवसमासशेषप्रकरणम्‌ ।</trailer>')
 	x = x.replace(u'<div type="SKsandarBaH">21<div type="DAtukramaH">7</div>9</div>',u'<div type="SKsandarBaH">2179</div>')
 	x = re.sub(u'<div type="prakaraRanAman">तद्धिकाधिकारप्रकरणे अपत्यादिविकारान्तार्थसाधारणप्रत्ययाः</div>\W+</div>\n</div>\n<div type="sUtra">',u'<div type="prakaraRanAman">तद्धिकाधिकारप्रकरणे अपत्यादिविकारान्तार्थसाधारणप्रत्ययाः</div>\n<div type="sUtra">',x)
-	x = re.sub(u'। इति तद्धिकाधिकारप्रकरणे अपत्यादिविकारान्तार्थसाधारणप्रत्ययाः ।',u'</div>\n</div>\n<div type="prakaraRAnta">तद्धिकाधिकारप्रकरणे अपत्यादिविकारान्तार्थसाधारणप्रत्ययाः</div>',x)
+	x = re.sub(u'। इति तद्धिकाधिकारप्रकरणे अपत्यादिविकारान्तार्थसाधारणप्रत्ययाः ।',u'</p>\n</div>\n<trailer>। इति तद्धिकाधिकारप्रकरणे अपत्यादिविकारान्तार्थसाधारणप्रत्ययाः ।</trailer>',x)
 	x = x.replace('<div type="DAtukramaH">226</div>5</div>','<div type="SKsandarBaH">2265</div>')
-	x = re.sub(u'</div>\n</div>\n<div type="prakaraRAnta">तिङन्तप्रत्ययमालाप्रकरणम्‌</div>',u'\n<div type="prakaraRAnta">तिङन्तप्रत्ययमालाप्रकरणम्‌</div>',x)
+	x = re.sub(u'<head>॥ अथ तिङन्तप्रत्ययमालाप्रकरणम्‌ ॥</head>',u'<head>॥ अथ तिङन्तप्रत्ययमालाप्रकरणम्‌ ॥</head>\n<p>',x)
+	x = re.sub(u'</p>\W+</div>\W+<trailer>। इति तिङन्तप्रत्ययमालाप्रकरणम्‌ ।</trailer>',u'</p>\W+<trailer>। इति तिङन्तप्रत्ययमालाप्रकरणम्‌ ।</trailer>',x)
 	x = re.sub(u'इत्थं वैदिकशब्दानां दिङ्मात्रमिह दर्शितम्‌ ।(\W+)तदस्तु प्रीतये श्रीमद्भवानीविश्वनाथयोः ॥\W+</div>\W+</body>',u'<div type="colophon">इत्थं वैदिकशब्दानां दिङ्मात्रमिह दर्शितम्‌ ।\g<1>तदस्तु प्रीतये श्रीमद्भवानीविश्वनाथयोः ॥</div>\n</div>\n</body>',x)
 	x = x.replace('</body>\n</text>\n</div>','</body>\n</text>\n</TEI>')
 	return x
