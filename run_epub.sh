ac7981cdf4d0ad22e15bc0e0c5c8cb8269c8b8ea
drdhaval2785 <drdhaval2785#gmail.com>
2017-03-26 08:34:14 +0530

    MIT notice on @kmadathil scripts

--- run_epub.sh
+++ run_epub.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Copyright 2017 Karthikeyan Madathil
+# Released under MIT licence. See LICENCE.txt.
+
 INFILE=docs/sk.html
 OUTFILE=docs/sk.epub
 ebook-convert $INFILE $OUTFILE --insert-blank-line --chapter "//h:h2" --chapter-mark "both" --level1-toc "//h:h2" --use-auto-toc --authors "Bhattoji Dikshita" --author-sort "Bhattoji Dikshita" --comments "Automatically generated from XML file: https://github.com/drdhaval2785/siddhantakaumudi" --language Sanskrit --title "Siddhanta Kaumudi" --max-toc-links 100
--- run_xslt.sh
+++ run_xslt.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Copyright 2017 Karthikeyan Madathil
+# Released under MIT licence. See LICENCE.txt.
 
 function run_xslt {
     xsltproc --debug --output $OUTFILE --encoding $ENCODING $XSLT $INFILE
